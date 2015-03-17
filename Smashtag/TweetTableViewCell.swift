af898a695cfb654e8d5471ad5db7474dcfe69447
pgore <pgore@adobe.com>
2015-03-17 00:09:19 -0700

    Adding custom UITableCell and auto alignment

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by pgore on 3/16/15.
+//  Copyright (c) 2015 Pradnyesh Gore. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
